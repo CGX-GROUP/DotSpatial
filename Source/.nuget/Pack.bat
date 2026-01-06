b2f0ab81b9066b4950a57150568e7a20593a6927
ViryBe <benoit.viry@laposte.net>
2026-01-06 10:07:39 +0100

    utilisation de la platform x64 au lieu de AnyCPU dans le pack de nugets (#46)
    Co-authored-by: Benoit VIRY <benoit.viry@cgx-group.com>

--- Source/.nuget/Pack.bat
+++ Source/.nuget/Pack.bat
@@ -5,22 +5,24 @@ if not defined NugetVersion ( set "NugetVersion=%NUGET_VERSION%" )
 if not defined PackageVersion ( set "PackageVersion=%NUGET_VERSION%" )
 set GdalVersion=3.12.0.57
 
+set PLATFORM=x64
+
 if not exist "..\packages\gdal_MSVC1936.%GdalVersion%" (
 	echo Not found gdal version %GdalVersion%
 	exit /b 1
 )
 
-nuget pack ..\DotSpatial.Controls\DotSpatial.Controls.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Data\DotSpatial.Data.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Data.Forms\DotSpatial.Data.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Extensions\DotSpatial.Extensions.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Modeling.Forms\DotSpatial.Modeling.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.NTSExtension\DotSpatial.NTSExtension.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Projections\DotSpatial.Projections.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Projections.Forms\DotSpatial.Projections.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Serialization\DotSpatial.Serialization.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Symbology\DotSpatial.Symbology.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Symbology.Forms\DotSpatial.Symbology.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Python\DotSpatial.Python.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
-nuget pack ..\DotSpatial.Data.Rasters.GdalExtension\DotSpatial.Data.Rasters.GdalExtension.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU";"GdalVersion=%GdalVersion%"
-nuget pack ..\DotSpatial.Plugins.Measure\DotSpatial.Plugins.Measure.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=AnyCPU"
+nuget pack ..\DotSpatial.Controls\DotSpatial.Controls.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Data\DotSpatial.Data.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Data.Forms\DotSpatial.Data.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Extensions\DotSpatial.Extensions.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Modeling.Forms\DotSpatial.Modeling.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.NTSExtension\DotSpatial.NTSExtension.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Projections\DotSpatial.Projections.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Projections.Forms\DotSpatial.Projections.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Serialization\DotSpatial.Serialization.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Symbology\DotSpatial.Symbology.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Symbology.Forms\DotSpatial.Symbology.Forms.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Python\DotSpatial.Python.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
+nuget pack ..\DotSpatial.Data.Rasters.GdalExtension\DotSpatial.Data.Rasters.GdalExtension.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%";"GdalVersion=%GdalVersion%"
+nuget pack ..\DotSpatial.Plugins.Measure\DotSpatial.Plugins.Measure.csproj -version "%NugetVersion%" -Properties "PackageVersion=%PackageVersion%";"Configuration=Release";"Platform=%PLATFORM%"
